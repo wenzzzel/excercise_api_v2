73b497cbc7d04eae13e42a2bf45b28ac178f3311
wenzzzel <wenzzzel@live.se>
2022-06-06 16:15:28 +0200

    Ran: dotnet new nunit -f net6.0

---
+++ excercise_api_v2.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace excercise_api_v2.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
