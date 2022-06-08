1ccb9ce56dc93c713218a30f1d7b052f8b8463ca
wenzzzel <wenzzzel@live.se>
2022-06-08 14:46:48 +0200

    Setup dependency injection for cosmosDB repo

--- excercise_api_v2/Program.cs
+++ excercise_api_v2/Program.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
+using Microsoft.Azure.Cosmos;
+using excercise_api_v2.Interfaces;
+using excercise_api_v2.Repos;
 
 var builder = WebApplication.CreateBuilder(args);
+var cosmosDbRepository = new CosmosDbRepository(new CosmosClient(builder.Configuration.GetConnectionString("CosmosDB")));
 
 // Add services to the container.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -12,6 +16,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ICosmosDbRepository>(cosmosDbRepository);
 
 var app = builder.Build();
 
